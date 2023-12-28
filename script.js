0cfbfb933aa57b30d6242727d3641c671a773e02
Baqar Ali <sayedalintl@gmail.com>
2023-12-28 16:41:26 +0530

    optimize notes

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
+let notes = JSON.parse(localStorage.getItem("notes")) || [];
+let addNotesContainer = document.querySelector('.addNotes-container');
+let input = document.getElementById('input');
+let index = -1;
 
-const addNotesContainer = document.querySelector('.addNotes-container');
-const input = document.getElementById('input');
+function renders(){
+  addNotesContainer.innerHTML = "";
+  notes.forEach(note=>{
+    let div = document.createElement('div');
+   div.classList.add('notes');
+   div.innerHTML = `${note.text} <i onclick="ondelete(${note.id})" id="delete" class="fa-solid fa-trash"></i>`;
+   addNotesContainer.appendChild(div);
+  }) 
+}
+renders()
 
-addNotesContainer.innerHTML = JSON.parse(localStorage.getItem("notes"));
-let addnotes = [];
 
 input.addEventListener('keypress',(e)=>{
   if(e.key === "Enter"){
-      let div = document.createElement('div');
-      div.classList.add('notes');
-      div.innerHTML = `${input.value} <i id="deletebtn" class="fa-solid fa-trash"></i>`;
-      addNotesContainer.appendChild(div);
-      input.value = " ";
-      let notes = localStorage.getItem("notes");
-      if(notes == null){
-       addnotes.push(addNotesContainer.innerHTML);
-       localStorage.setItem("notes",JSON.stringify(addnotes))
-      }
-      else{
-        addnotes.push(addNotesContainer.innerHTML);
-       localStorage.setItem("notes",JSON.stringify(addnotes))
-      }
-      
-      
-    }
+    index++;
+   notes.push({id:index,text:input.value});
+   renders()
+   
+  localStorage.setItem("notes",JSON.stringify(notes));
+  input.value = "";
+  }
 })
 
-const notesLists = document.querySelectorAll('.notes')
-notesLists.forEach(noteslist=>{
-    noteslist.addEventListener('click',(e)=>{
-if(e.target.classList.contains('fa-solid')){
-   noteslist.remove()
+
+function ondelete(i){
+  notes = notes.filter(note=>note.id !== i);
+  localStorage.setItem("notes",JSON.stringify(notes))
 }
-    })
-})
+
+
+
+
+
 
 
 
