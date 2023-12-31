b5454e5d5bc880bcaf59c35b4a2bc67cacd049c8
Baqar Ali <sayedalintl@gmail.com>
2023-12-31 17:16:25 +0530

    complete note project

--- script.js
+++ script.js
@@ -1,36 +1,80 @@
-let notes = JSON.parse(localStorage.getItem("notes")) || [];
+
 let addNotesContainer = document.querySelector('.addNotes-container');
 let input = document.getElementById('input');
-let index = -1;
+let str = "";
 
-function renders(){
-  addNotesContainer.innerHTML = "";
-  notes.forEach(note=>{
-    let div = document.createElement('div');
-   div.classList.add('notes');
-   div.innerHTML = `${note.text} <i onclick="ondelete(${note.id})" id="delete" class="fa-solid fa-trash"></i>`;
-   addNotesContainer.appendChild(div);
-  }) 
+
+const ondelete =(i) =>{
+  let notedata = JSON.parse(localStorage.getItem("notes"));
+   notedata = notedata.filter(note=>note != i);
+   localStorage.setItem("notes",JSON.stringify(notedata))
+   showNotes()
+} 
+
+const onEdit = (i) =>{
+  input.value = i;
+  console.log(input.value);
+  let edit = true;
+  
+  let notedata = JSON.parse(localStorage.getItem("notes"));
+  let finduserInput = notedata.find(note=>note === input.value);
+  if(edit){
+    input.addEventListener('keypress',(e)=>{
+      if(e.key === "e" || e.key === "E"){
+        notedata = JSON.parse(localStorage.getItem("notes"));
+        if(input.value){
+          notedata.splice(finduserInput,1,input.value)
+
+          localStorage.setItem("notes",JSON.stringify(notedata));
+           showNotes()
+        }
+        
+      }
+      input.value;
+    })
+    
+  }
+  
+
 }
-renders()
+
+const showNotes = () =>{
+  let notesdata = JSON.parse(localStorage.getItem("notes"));
+  
+  if(notesdata){
+    addNotesContainer.innerHTML = "";
+  notesdata.forEach(note=>{
+    str = `<p class="notes">${note}<i onclick="ondelete('${note}')" class="fa-solid fa-delete-left"></i><i onclick="onEdit('${note}')" class="fa-solid fa-pen"></i></p>`;
+    addNotesContainer.innerHTML += str;
+  });
+  
+}
+ else{
+  addNotesContainer.innerHTML = "<p class='notes'>No Notes here</p>";
+ }
+  
+}
+showNotes()
 
 
 input.addEventListener('keypress',(e)=>{
-  if(e.key === "Enter"){
-    index++;
-   notes.push({id:index,text:input.value});
-   renders()
-   
-  localStorage.setItem("notes",JSON.stringify(notes));
-  input.value = "";
+  if(e.key == "Enter"){
+    let notes = localStorage.getItem("notes");
+    if(notes == null){
+      let json = [];
+      json.push(input.value);
+      localStorage.setItem("notes",JSON.stringify(json))
+    }
+    else{
+      let json = JSON.parse(localStorage.getItem("notes"));
+      json.push(input.value);
+      localStorage.setItem("notes",JSON.stringify(json))
+    }
+    showNotes()
+    input.value = "";
+    
   }
-})
-
-
-function ondelete(i){
-  notes = notes.filter(note=>note.id !== i);
-  localStorage.setItem("notes",JSON.stringify(notes))
-}
+});
 
 
 
